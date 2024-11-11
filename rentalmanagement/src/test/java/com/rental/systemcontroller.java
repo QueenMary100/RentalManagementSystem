c8ec6fa5bf451a9c3852064383ec2120aa22b2ce
Mary Syokau <160385341+QueenMary100@users.noreply.github.com>
2024-11-11 22:35:45 +0300

    Add files via upload

---
+++ rentalmanagement/src/test/java/com/rental/systemcontroller.java
@@ -0,0 +1,35 @@
+package com.rental.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.rental.RentalmanagementApplication;
+
+
+@Controller
+public class systemcontroller {
+	
+	@GetMapping("/")
+	public String showHome(Model model) {
+		String[][] result = new RentalmanagementApplication().showHouses();
+		System.out.println(result[0][0]);
+		model.addAttribute("houses", result);
+
+		return "home";
+	}
+	@GetMapping("/westlands")
+	public String getWestlandsPage(Model model) {
+		String vacancies = new RentalmanagementApplication().getVacancies();
+	    model.addAttribute("vacancies", vacancies);
+		System.out.println(vacancies);
+
+		List<String> locations = new RentalmanagementApplication().getLocation();
+		model.addAttribute("locations", locations);
+
+	    return "Westlands"; // Must match the template name in src/main/resources/templates
+	}
+	
+}
